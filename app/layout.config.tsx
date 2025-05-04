5b7eeccdf29a903b5fbe37796e1f3fb52a110603
joeBlockchain <94577719+joeBlockchain@users.noreply.github.com>
2025-05-03 17:08:40 -0700

    initial commit

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,5 +1,5 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-
+import { Layers } from 'lucide-react';
 /**
  * Shared layout configurations
  *
@@ -11,15 +11,10 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
+        <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-neutral-800 text-neutral-100">
+          <Layers className="h-5 w-5" />
+        </div>
+        myStax
       </>
     ),
   },
